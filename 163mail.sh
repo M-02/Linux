3cf74d8d94935516841a9d7147c99dc578b68d44
M-02 <34387208+M-02@users.noreply.github.com>
2020-11-04 11:32:04 +0800

    Update 163mail.sh

--- 163mail.sh
+++ 163mail.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/bash
 #cent OS自动配置163邮箱并发送测试邮件
-read -p "输入你的邮件地址:" m1
+read -p "输入你的邮件地址:" add1
 read -p "输入你的邮件密码:" password
 yum install -y mailx
 mkdir -p /root/.certs
 echo "# mail config
-set from=$m1
+set from=$add1
 set smtp=smtps://smtp.163.com:465  
-set smtp-auth-user=$m1
+set smtp-auth-user=$add1
 set smtp-auth-password=$password 
 set smtp-auth=login 
 set nss-config-dir=/root/.certs  
@@ -17,7 +17,7 @@ certutil -A -n "GeoTrust SSL CA" -t "C,," -d ~/.certs -i ~/.certs/163.crt
 certutil -A -n "GeoTrust Global CA" -t "C,," -d ~/.certs -i ~/.certs/163.crt
 cd /root/.certs/
 certutil -A -n "GeoTrust SSL CA - G3" -t "Pu,Pu,Pu" -d ./ -i 163.crt
-read -p "输入你想发送的收件人:" m2
-read -p "输入你想发送的信息标题:" tl
-read -p "输入你想发送的信息:" ms
-echo $tl | mail -v -s $ms $m2
+read -p "输入你想发送的收件人:" add2
+read -p "输入你想发送的信息标题:" title
+read -p "输入你想发送的信息:" message
+echo $message | mail -v -s $title $add2
